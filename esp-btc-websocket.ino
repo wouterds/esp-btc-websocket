4efb5482298983b2a5295d696331047f53d5748d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-02-27 12:25:58 +0100

    :construction: Display test

--- esp-btc-websocket.ino
+++ esp-btc-websocket.ino
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <SevenSegmentTM1637.h>
 #include "env.h"
+
+SevenSegmentTM1637 display(PIN_CLK, PIN_DIO);
 
 void setup() {
   Serial.begin(115200);
@@ -19,8 +22,19 @@ void setup() {
 
   Serial.print("[WiFi] Connected to network, IP: ");
   Serial.println(WiFi.localIP());
+
+  Serial.println("[Display] Setup");
+  display.begin(6);
+  display.setBacklight(20);
+  display.clear();
 }
 
 void loop() {
+  display.print('654321');
+  delay(2000);
+  display.clear();
+
+  display.print('000000');
   delay(1000);
+  display.clear();
 }
