519aa75586fe6209c3ce1d522b6f7f11d0f9e553
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-03-01 15:24:19 +0100

    :sparkles: Bitcoin price

--- esp-btc-websocket.ino
+++ esp-btc-websocket.ino
@@ -1,50 +1,72 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <TM1637Display.h>
+#include <ArduinoWebsockets.h>
+#include <ArduinoJson.h>
 #include "env.h"
 
+using namespace websockets;
+
 TM1637Display display(PIN_CLK, PIN_DIO, DEFAULT_BIT_DELAY, 6);
+WebsocketsClient websocket;
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
 
-  Serial.print("[WiFi] Connecting to: ");
+  Serial.print(F("[WiFi] Connecting to: "));
   Serial.print(WIFI_SSID);
-  Serial.print("..");
+  Serial.print(F(".."));
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.print(F("."));
   }
   Serial.println();
 
-  Serial.print("[WiFi] Connected to network, IP: ");
+  Serial.print(F("[WiFi] Connected to network, IP: "));
   Serial.println(WiFi.localIP());
 
-  Serial.println("[Display] Setup");
+  Serial.println(F("[Display] Setup"));
   display.setBrightness(0x0f);
   display.clear();
+
+  Serial.println(F("[WebSocket] Setup"));
+
+  bool connected = websocket.connect(F("wss://fstream.binance.com/stream?streams=btcusdt@markPrice"));
+  if (connected) {
+      Serial.println(F("[WebSocket] Connected!"));
+      websocket.ping();
+  } else {
+      Serial.println(F("[WebSocket] Not connected!"));
+  }
+
+  websocket.onMessage([&](WebsocketsMessage message){
+      Serial.println(F("Got Message!"));
+      Serial.println(message.data());
+
+      StaticJsonDocument<512> doc;
+      DeserializationError error = deserializeJson(doc, message.data());
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+      }
+
+      int price = doc["data"]["p"];
+      Serial.print(F("Serialized json! Price:"));
+      Serial.println(price);
+      showNumberDec(price);
+  });
 }
 
 void loop() {
-  showNumberDec(3);
-  delay(1000);
-  showNumberDec(2);
-  delay(1000);
-  showNumberDec(1);
-  delay(1000);
-
-  Serial.println("Starting counter!");
-  for (int i = 1; i <= 999999; i++) {
-    Serial.print("count: ");
-    Serial.println(i);
-    showNumberDec(i);
-    delay(10);
+  if (websocket.available()) {
+    websocket.poll();
   }
-
-  delay(3000);
+  delay(100);
 }
 
 uint8_t previousDigits = 1;
