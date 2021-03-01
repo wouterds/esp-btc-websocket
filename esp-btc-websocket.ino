f0de5ef9105d55e2523434677c2d072552f32369
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-03-01 11:58:51 +0100

    :sparkles: Try out other lib

--- esp-btc-websocket.ino
+++ esp-btc-websocket.ino
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <SevenSegmentTM1637.h>
+// #include <SevenSegmentTM1637.h>
+#include <TM1637Display.h>
 #include "env.h"
 
-SevenSegmentTM1637 display(PIN_CLK, PIN_DIO);
+// SevenSegmentTM1637 display(PIN_CLK, PIN_DIO);
+TM1637Display display(PIN_CLK, PIN_DIO);
 
 void setup() {
   Serial.begin(115200);
@@ -24,17 +26,41 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   Serial.println("[Display] Setup");
-  display.begin(6);
-  display.setBacklight(20);
+  // display.begin(6);
+  // display.setBacklight(20);
+  // display.clear();
+  display.setBrightness(0x0f);
   display.clear();
 }
 
 void loop() {
-  display.print('654321');
-  delay(2000);
+  // display.print('654321');
+  // delay(2000);
+  // display.clear();
+  //
+  // display.print('000000');
   display.clear();
-
-  display.print('000000');
+  display.showNumberDec(1, false, 1, 0);
   delay(1000);
+
   display.clear();
+  display.showNumberDec(1, false, 1, 1);
+  delay(1000);
+
+  display.clear();
+  display.showNumberDec(1, false, 1, 2);
+  delay(1000);
+
+  display.clear();
+  display.showNumberDec(1, false, 1, 3);
+  delay(1000);
+
+  display.clear();
+  display.showNumberDec(1, false, 1, 4);
+  delay(1000);
+
+  display.clear();
+  display.showNumberDec(1, false, 1, 5);
+  delay(1000);
+  // display.clear();
 }
